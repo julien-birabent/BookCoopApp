6fea4dd6277050b4c2800e638b96e7817245e75a
Julien Birabent <jul.birabent@gmail.com>
2016-10-22 19:52:51 -0400

    Ajout de l'activité StudentActivity et de son layout activity_student

--- app/src/main/java/com/julienbirabent/bookcoopapp/LoginActivity.java
+++ app/src/main/java/com/julienbirabent/bookcoopapp/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.julienbirabent.bookcoopapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import model.Student;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,7 +30,8 @@ public class LoginActivity extends AppCompatActivity {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this, StudentActivity.class);
+                startActivity(intent);
             }
         });
     }
