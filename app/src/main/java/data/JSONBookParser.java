d9bcd9f785b321260dcf5fc398a4435faf20a2de
Julien Birabent <jul.birabent@gmail.com>
2016-10-23 20:21:46 -0400

    Création de JSONBookParser et de deux squelettes de méthodes permettant de traduire une String représentant des objet JSON de la classe Book en objet physique dans notre application.

---
+++ app/src/main/java/data/JSONBookParser.java
@@ -0,0 +1,37 @@
+package data;
+
+import java.util.ArrayList;
+
+import model.Book;
+
+/**
+ * Created by Julien on 23/10/2016.
+ */
+
+public class JSONBookParser {
+
+
+    public JSONBookParser() {
+    }
+
+    /**
+     * Méthode pour décomposer une string représentant un objet JSON en un objet Book.
+     * @param contents
+     * @return
+     */
+    public Book parseBook(String contents){
+
+        return null;
+    }
+
+    /**
+     * Méthode pour décomposer une string représentant un ensemble d'objet Book.
+     * @param contents
+     * @return
+     */
+    public ArrayList<Book> parseManyBooks(String contents){
+
+        return null;
+    }
+
+}
