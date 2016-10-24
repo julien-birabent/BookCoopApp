e5a964af555ce8bf947158b2e279f655bb970add
Julien Birabent <jul.birabent@gmail.com>
2016-10-24 09:16:34 -0400

    JSONBookParser : - complétion de la méthode parseBook( book : Book ) : Book

--- app/src/main/java/data/JSONBookParser.java
+++ app/src/main/java/data/JSONBookParser.java
@@ -1,8 +1,12 @@
 package data;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 import model.Book;
+import utils.JSONUtils;
 
 /**
  * Created by Julien on 23/10/2016.
@@ -19,9 +23,28 @@ public class JSONBookParser {
      * @param contents
      * @return
      */
-    public Book parseBook(String contents){
+    public Book parseBook(String contents)
+    {
+        Book book = new Book();
 
-        return null;
+        try {
+
+            // On convertit la chaîne de caractère représentant l'objetJSON en objet JSON
+            JSONObject objBook = new JSONObject(contents);
+            // On remplit les champs de l'instance de Book avant de la retourner
+            book.setAuthor(JSONUtils.getString("author", objBook));
+            book.setTitle(JSONUtils.getString("title",objBook));
+            book.setIsbn(JSONUtils.getInt("isbn",objBook));
+            book.setNbPages(JSONUtils.getInt("page_count",objBook));
+            book.setPrice(JSONUtils.getFloat("mint_price",objBook));
+
+            return book;
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        return book;
     }
 
     /**
