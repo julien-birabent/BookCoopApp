512b41cd61bb7bd471b03dbf2618baf50294cb15
Julien Birabent <jul.birabent@gmail.com>
2016-11-17 14:28:43 -0500

    COmplétion de la méthode parseCopy et ajout de constantes définissant les champs des objets JSON à extraire

--- app/src/main/java/data/JSONCopyParser.java
+++ app/src/main/java/data/JSONCopyParser.java
@@ -1,8 +1,11 @@
 package data;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 import model.Copy;
+import utils.JSONUtils;
 
 /**
  * Created by Julien on 2016-11-16.
@@ -10,6 +13,11 @@ import model.Copy;
 
 public class JSONCopyParser {
 
+
+    public final static String BOOK_ID = "book_id";
+    public final static String COPY_ID = "id";
+    public final static String INTEGRITY ="integrity";
+    public final static String ID ="$oid";
 
     public JSONCopyParser() {
 
@@ -22,6 +30,28 @@ public class JSONCopyParser {
     public Copy parseCopy(String contents){
 
         Copy copy = new Copy();
+        try {
+            // On convertit la chaîne de caractère représentant l'objet JSON en objet JSON
+            JSONObject objCopy = new JSONObject(contents);
+
+            // On récupère l'objet JSON contenant l'id du Book associée à la copy.
+            JSONObject objBookId = JSONUtils.getObject(BOOK_ID,objCopy);
+            // On attribut l'id du Book associé à Copy
+            copy.setCopyId(JSONUtils.getInt(ID,objBookId));
+
+            // On récupère l'objet JSON contenant l'id de la Copy
+            JSONObject objCopyId = JSONUtils.getObject(COPY_ID,objCopy);
+            // On extrait l'id de la Copy contenu dans l'objet précédemment extrait.
+            copy.setCopyId(JSONUtils.getInt(ID,objCopyId));
+
+            copy.setPhysicalState(JSONUtils.getString(INTEGRITY,objCopy));
+
+            //copy.setBookId(JSONUtils.getInt(BOOK_ID,objCopy));
+           // copy.setCopyId(JSONUtils.getInt(COPY_ID,objCopy));
+        }
+        catch(Exception e ){
+
+        }
 
         return copy;
     }
@@ -34,6 +64,9 @@ public class JSONCopyParser {
     public ArrayList<Copy> parseManyCopies(String contents){
 
         ArrayList<Copy> copyArrayList = new ArrayList<Copy>();
+
+
+
 
         return copyArrayList;
     }
